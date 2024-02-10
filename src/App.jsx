78adba257350f721447d9294f00a68a6a7f0459d
Dikens27 <kar.karovich321@gmail.com>
2024-02-10 12:03:15 +0200

    task-2

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import {
   ForbesList,
 } from 'components';
 import article from './data/article.json';
+import statistics from './data/stats.json'
 
 export const App = () => {
   return (
@@ -17,7 +18,7 @@ export const App = () => {
         <BlogCard article={article} />
 
         <Heading title="Task 2 Statistics" top bottom />
-        <Statistics title="Main Statistics" />
+        <Statistics title="Main Statistics" statistics={statistics} />
 
         <Heading title="Task 3 Forbes list" top bottom />
         <ForbesList />
